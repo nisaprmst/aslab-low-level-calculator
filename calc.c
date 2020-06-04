d9a414b94a69e7c8047cfeadd57df8363346767c
nisaprmst <nisaprmst@gmail.com>
2020-06-04 14:30:24 +0700

    simplified + and -

--- calc.c
+++ calc.c
@@ -43,21 +43,13 @@ int main(int argc, char* argv[]){
 			// reg3 = reg2 + reg2
 			// ingat reg3 ga bersih jd harus ditangani semua kondisi
 			reg3.bit1 = 0;
-			if (reg2.bit1 == 1) reg3.bit2 = 1;
-			else reg3.bit2 = 0;
-			if (reg2.bit2 == 1) reg3.bit3 = 1;
-			else reg3.bit3 = 0;
-			if (reg2.bit3 == 1) reg3.bit4 = 1;
-			else reg3.bit4 = 0;
-			if (reg2.bit4 == 1) reg3.bit5 = 1;
-			else reg3.bit5 = 0;
-			if (reg2.bit5 == 1) reg3.bit6 = 1;
-			else reg3.bit6 = 0;
-			if (reg2.bit6 == 1) reg3.bit7 = 1;
-			else reg3.bit7 = 0;
-			if (reg2.bit7 == 1) reg3.bit8 = 1;
-			else reg3.bit8 = 0;
-
+			reg3.bit2 = reg2.bit1;
+			reg3.bit3 = reg2.bit2;
+			reg3.bit4 = reg2.bit3;
+			reg3.bit5 = reg2.bit4;
+			reg3.bit6 = reg2.bit5;
+			reg3.bit7 = reg2.bit6;
+			reg3.bit8 = reg2.bit7;
 			// kalo c bukan digit pertama / tidak semuanya = 0
 			if (!(reg2.bit1 == 0 && reg2.bit2 == 0
 				&& reg2.bit3 == 0 && reg2.bit4 == 0
@@ -102,7 +94,6 @@ int main(int argc, char* argv[]){
 					reg3.bit4 = 1;
 					reg3.bit1 = 1;
 				}
-				p = 0;
 				goto fix_reg2;
 			update_reg2:
 				// shift kiri 3
@@ -117,168 +108,37 @@ int main(int argc, char* argv[]){
 				// tambah reg3
 			fix_reg2:
 				// reg2 += reg3
-				if (reg3.bit1 == 1) {
-					// karena reg3.bit1 udh ga kepake bisa dipake buat nyimpen angka kalo 1 + 1
-					if (reg2.bit1 == 0) {
-						reg2.bit1 = 1; // update reg2
-						reg3.bit1 = 0; // ga nyimpen
-					} else {
-						reg2.bit1 = 0; // nyimpen di reg3.bit1 = 1
-					}
+				if (reg3.bit1) { // karena reg3.bit1 udh ga kepake bisa dipake buat nyimpen angka kalo 1 + 1
+					reg3.bit1 = reg2.bit1; // nyimpen / ga
+					reg2.bit1 = ~reg2.bit1; //update reg2
 				}
-				if (reg3.bit2 == 1) {
-					// karena reg3.bit1 udh ga kepake bisa dipake buat nyimpen angka kalo 1 + 1
-					if (reg2.bit2 == 0) {
-						// cek nyimpen apa ga, kalo nyimpen biarin 
-						// krn reg3.bit2 udh bener isinya 1 (nyimpen) terus reg2.bit2 udh bener isinya 0
-						// kalo ga nyimpen brrti update
-						if (reg3.bit1 == 0) {
-							reg2.bit2 = 1; // update reg2
-							reg3.bit2 = 0; // ga nyimpen
-						}
-					} else { // reg2.bit2 == 1
-						// cek nyimpen apa ga, kalo ga brrti kyk biasa
-						if (reg3.bit1 == 0) {
-							reg2.bit2 = 0; // nyimpen di reg3
-						}
-						// kalo nyimpen brrti gaada yg diupdate krn reg3 udh bener nyimpen
-						// terus reg2 udh bener yaitu nilainya 1
-					}
-				} else {	
-					if (reg3.bit1 == 1) {
-						if (reg2.bit2 == 1) {
-							reg2.bit2 = 0;
-							reg3.bit2 = 1;
-						} else {
-							reg2.bit2 = 1;
-						}
-					}
+				if (reg3.bit2 != reg3.bit1)  {
+					reg3.bit2 = reg2.bit2; // nyimpen / ga
+					reg2.bit2 = ~reg2.bit2; //update reg2
 				}
-				if (reg3.bit3 == 1) {
-					if (reg2.bit3 == 0) {
-						if (reg3.bit2 == 0) {
-							reg2.bit3 = 1; // update reg2
-							reg3.bit3 = 0; // ga nyimpen
-						}
-					} else {
-						if (reg3.bit2 == 0) {
-							reg2.bit3 = 0; // nyimpen di reg3
-						}
-					}
-				} else {
-					if (reg3.bit2 == 1) {
-						if (reg2.bit3 == 1) {
-							reg2.bit3 = 0;
-							reg3.bit3 = 1;
-						} else {
-							reg2.bit3 = 1;
-						}
-					}
+				if (reg3.bit3 != reg3.bit2)  {
+					reg3.bit3 = reg2.bit3; // nyimpen / ga
+					reg2.bit3 = ~reg2.bit3; //update reg2
 				}
-				if (reg3.bit4 == 1) {
-					if (reg2.bit4 == 0) {
-						if (reg3.bit3 == 0) {
-							reg2.bit4 = 1; // update reg2
-							reg3.bit4 = 0; // ga nyimpen
-						}
-					} else { 
-						if (reg3.bit3 == 0) {
-							reg2.bit4 = 0; // nyimpen di reg3
-						}
-					}
-				} else {
-					if (reg3.bit3 == 1) {
-						if (reg2.bit4 == 1) {
-							reg2.bit4 = 0;
-							reg3.bit4 = 1;
-						} else {
-							reg2.bit4 = 1;
-						}
-					}
+				if (reg3.bit4 != reg3.bit3)  {
+					reg3.bit4 = reg2.bit4; // nyimpen / ga
+					reg2.bit4 = ~reg2.bit4; //update reg2
 				}
-				if (reg3.bit5 == 1) {
-					if (reg2.bit5 == 0) {
-						if (reg3.bit4 == 0) {
-							reg2.bit5 = 1; // update reg2
-							reg3.bit5 = 0; // ga nyimpen
-						}
-					} else {
-						if (reg3.bit4 == 0) {
-							reg2.bit5 = 0; // nyimpen di reg3
-						}
-					}
-				} else {
-					if (reg3.bit4 == 1) {
-						if (reg2.bit5 == 1) {
-							reg2.bit5 = 0;
-							reg3.bit5 = 1;
-						} else {
-							reg2.bit5 = 1;
-						}
-					}
+				if (reg3.bit5 != reg3.bit4)  {
+					reg3.bit5 = reg2.bit5; // nyimpen / ga
+					reg2.bit5 = ~reg2.bit5; //update reg2
 				}
-				if (reg3.bit6 == 1) {
-					if (reg2.bit6 == 0) {
-						if (reg3.bit5 == 0) {
-							reg2.bit6 = 1; // update reg2
-							reg3.bit6 = 0; // ga nyimpen
-						}
-					} else {
-						if (reg3.bit5 == 0) {
-							reg2.bit6 = 0; // nyimpen di reg3
-						}
-					}
-				} else {
-					if (reg3.bit5 == 1) {
-						if (reg2.bit6 == 1) {
-							reg2.bit6 = 0;
-							reg3.bit6 = 1;
-						} else {
-							reg2.bit6 = 1;
-						}
-					}
+				if (reg3.bit6 != reg3.bit5)  {
+					reg3.bit6 = reg2.bit6; // nyimpen / ga
+					reg2.bit6 = ~reg2.bit6; //update reg2
 				}
-				if (reg3.bit7 == 1) {
-					if (reg2.bit7 == 0) {
-						if (reg3.bit6 == 0) {
-							reg2.bit7 = 1; // update reg2
-							reg3.bit7 = 0; // ga nyimpen
-						}
-					} else {
-						if (reg3.bit6 == 0) {
-							reg2.bit7 = 0; // nyimpen di reg3
-						}
-					}
-				} else {
-					if (reg3.bit6 == 1) {
-						if (reg2.bit7 == 1) {
-							reg2.bit7 = 0;
-							reg3.bit7 = 1;
-						} else {
-							reg2.bit7 = 1;
-						}
-					}
-				}
-				if (reg3.bit8 == 1) {
-					if (reg2.bit8 == 0) {
-						if (reg3.bit7 == 0) {
-							reg2.bit8 = 1; // update reg2
-							reg3.bit8 = 0; // ga nyimpen
-						}
-					} else {
-						if (reg3.bit7 == 0) {
-							reg2.bit8 = 0; // nyimpen di reg3
-						}
-					}
-				} else {
-					if (reg3.bit7 == 1) {
-						if (reg2.bit8 == 1) {
-							reg2.bit8 = 0;
-							reg3.bit8 = 1;
-						} else {
-							reg2.bit8 = 1;
-						}
-					}
+				if (reg3.bit7 != reg3.bit6)  {
+					reg3.bit7 = reg2.bit7; // nyimpen / ga
+					reg2.bit7 = ~reg2.bit7; //update reg2
+				} 
+				if (reg3.bit8 != reg3.bit7)  {
+					reg3.bit8 = reg2.bit8; // nyimpen / ga
+					reg2.bit8 = ~reg2.bit8; //update reg2
 				}
 			if (reg4.bit5 == 1) {
 				reg4.bit5 = 0;
@@ -300,326 +160,71 @@ int main(int argc, char* argv[]){
 			if (reg4.bit1 == 1) { //operasi +
 				// reg1 += reg2
 				tambah:
-				if (reg2.bit1 == 1) {
-					// karena reg2.bit1 udh ga kepake bisa dipake buat nyimpen angka kalo 1 + 1
-					if (reg1.bit1 == 0) {
-						reg1.bit1 = 1; // update reg1
-						reg2.bit1 = 0; // ga nyimpen
-					} else {
-						reg1.bit1 = 0; // nyimpen di reg2.bit1 = 1
-					}
+				if (reg2.bit1) { // karena reg2.bit1 udh ga kepake bisa dipake buat nyimpen angka kalo 1 + 1
+					reg2.bit1 = reg1.bit1; // nyimpen / ga
+					reg1.bit1 = ~reg1.bit1; //update reg1
 				}
-				if (reg2.bit2 == 1) {
-					// karena reg2.bit1 udh ga kepake bisa dipake buat nyimpen angka kalo 1 + 1
-					if (reg1.bit2 == 0) {
-						// cek nyimpen apa ga, kalo nyimpen biarin 
-						// krn reg2.bit2 udh bener isinya 1 (nyimpen) terus reg1.bit2 udh bener isinya 0
-						// kalo ga nyimpen brrti update
-						if (reg2.bit1 == 0) {
-							reg1.bit2 = 1; // update reg1
-							reg2.bit2 = 0; // ga nyimpen
-						}
-					} else { // reg1.bit2 == 1
-						// cek nyimpen apa ga, kalo ga brrti kyk biasa
-						if (reg2.bit1 == 0) {
-							reg1.bit2 = 0; // nyimpen di reg2
-						}
-						// kalo nyimpen brrti gaada yg diupdate krn reg2 udh bener nyimpen
-						// terus reg1 udh bener yaitu nilainya 1
-					}
-				} else {
-					
-					if (reg2.bit1 == 1) {
-						if (reg1.bit2 == 1) {
-							reg1.bit2 = 0;
-							reg2.bit2 = 1;
-						} else {
-							reg1.bit2 = 1;
-						}
-					}
+				if (reg2.bit2 != reg2.bit1)  {
+					reg2.bit2 = reg1.bit2; // nyimpen / ga
+					reg1.bit2 = ~reg1.bit2; //update reg1
 				}
-				if (reg2.bit3 == 1) {
-					if (reg1.bit3 == 0) {
-						if (reg2.bit2 == 0) {
-							reg1.bit3 = 1; // update reg1
-							reg2.bit3 = 0; // ga nyimpen
-						}
-					} else {
-						if (reg2.bit2 == 0) {
-							reg1.bit3 = 0; // nyimpen di reg2
-						}
-					}
-				} else {
-					if (reg2.bit2 == 1) {
-						if (reg1.bit3 == 1) {
-							reg1.bit3 = 0;
-							reg2.bit3 = 1;
-						} else {
-							reg1.bit3 = 1;
-						}
-					}
+				if (reg2.bit3 != reg2.bit2)  {
+					reg2.bit3 = reg1.bit3; // nyimpen / ga
+					reg1.bit3 = ~reg1.bit3; //update reg1
 				}
-				if (reg2.bit4 == 1) {
-					if (reg1.bit4 == 0) {
-						if (reg2.bit3 == 0) {
-							reg1.bit4 = 1; // update reg1
-							reg2.bit4 = 0; // ga nyimpen
-						}
-					} else { 
-						if (reg2.bit3 == 0) {
-							reg1.bit4 = 0; // nyimpen di reg2
-						}
-					}
-				} else {
-					if (reg2.bit3 == 1) {
-						if (reg1.bit4 == 1) {
-							reg1.bit4 = 0;
-							reg2.bit4 = 1;
-						} else {
-							reg1.bit4 = 1;
-						}
-					}
+				if (reg2.bit4 != reg2.bit3)  {
+					reg2.bit4 = reg1.bit4; // nyimpen / ga
+					reg1.bit4 = ~reg1.bit4; //update reg1
 				}
-				if (reg2.bit5 == 1) {
-					if (reg1.bit5 == 0) {
-						if (reg2.bit4 == 0) {
-							reg1.bit5 = 1; // update reg1
-							reg2.bit5 = 0; // ga nyimpen
-						}
-					} else {
-						if (reg2.bit4 == 0) {
-							reg1.bit5 = 0; // nyimpen di reg2
-						}
-					}
-				} else {
-					if (reg2.bit4 == 1) {
-						if (reg1.bit5 == 1) {
-							reg1.bit5 = 0;
-							reg2.bit5 = 1;
-						} else {
-							reg1.bit5 = 1;
-						}
-					}
+				if (reg2.bit5 != reg2.bit4)  {
+					reg2.bit5 = reg1.bit5; // nyimpen / ga
+					reg1.bit5 = ~reg1.bit5; //update reg1
 				}
-				if (reg2.bit6 == 1) {
-					if (reg1.bit6 == 0) {
-						if (reg2.bit5 == 0) {
-							reg1.bit6 = 1; // update reg1
-							reg2.bit6 = 0; // ga nyimpen
-						}
-					} else {
-						if (reg2.bit5 == 0) {
-							reg1.bit6 = 0; // nyimpen di reg2
-						}
-					}
-				} else {
-					if (reg2.bit5 == 1) {
-						if (reg1.bit6 == 1) {
-							reg1.bit6 = 0;
-							reg2.bit6 = 1;
-						} else {
-							reg1.bit6 = 1;
-						}
-					}
+				if (reg2.bit6 != reg2.bit5)  {
+					reg2.bit6 = reg1.bit6; // nyimpen / ga
+					reg1.bit6 = ~reg1.bit6; //update reg1
 				}
-				if (reg2.bit7 == 1) {
-					if (reg1.bit7 == 0) {
-						if (reg2.bit6 == 0) {
-							reg1.bit7 = 1; // update reg1
-							reg2.bit7 = 0; // ga nyimpen
-						}
-					} else {
-						if (reg2.bit6 == 0) {
-							reg1.bit7 = 0; // nyimpen di reg2
-						}
-					}
-				} else {
-					if (reg2.bit6 == 1) {
-						if (reg1.bit7 == 1) {
-							reg1.bit7 = 0;
-							reg2.bit7 = 1;
-						} else {
-							reg1.bit7 = 1;
-						}
-					}
+				if (reg2.bit7 != reg2.bit6)  {
+					reg2.bit7 = reg1.bit7; // nyimpen / ga
+					reg1.bit7 = ~reg1.bit7; //update reg1
+				} 
+				if (reg2.bit8 != reg2.bit7)  {
+					reg2.bit8 = reg1.bit8; // nyimpen / ga
+					reg1.bit8 = ~reg1.bit8; //update reg1
 				}
-				if (reg2.bit8 == 1) {
-					if (reg1.bit8 == 0) {
-						if (reg2.bit7 == 0) {
-							reg1.bit8 = 1; // update reg1
-							reg2.bit8 = 0; // ga nyimpen
-						}
-					} else {
-						if (reg2.bit7 == 0) {
-							reg1.bit8 = 0; // nyimpen di reg2
-						}
-					}
-				} else {
-					if (reg2.bit7 == 1) {
-						if (reg1.bit8 == 1) {
-							reg1.bit8 = 0;
-							reg2.bit8 = 1;
-						} else {
-							reg1.bit8 = 1;
-						}
-					}
-				}
-			} else if (reg4.bit2 == 1) {
+			} else if (reg4.bit2 == 1) { //operasi -
 				kurang:
-				if (reg2.bit1 == 1) {
-					if (reg1.bit1 == 0) {
-						reg1.bit1 = 1; // update reg1
-					} else {
-						reg1.bit1 = 0; // nyimpen di reg2.bit1 = 1
-						reg2.bit1 = 0;
-					}
+				if (reg2.bit1) { // karena reg2.bit1 udh ga kepake bisa dipake buat nyimpen angka kalo 1 + 1
+					reg2.bit1 = ~reg1.bit1; // nyimpen / ga
+					reg1.bit1 = ~reg1.bit1; //update reg1
 				}
-				if (reg2.bit2 == 1) {
-					if (reg1.bit2 == 0) {
-						if (reg2.bit1 == 0) {
-							reg1.bit2 = 1; // update reg1
-						}
-					} else { // reg1.bit2 == 1
-						if (reg2.bit1 == 0) {
-							reg1.bit2 = 0; // nyimpen di reg2
-							reg2.bit2 = 0;
-						}
-					}
-				} else {
-					if (reg2.bit1 == 1) {
-						if (reg1.bit2 == 1) {
-							reg1.bit2 = 0;
-						} else {
-							reg2.bit2 = 1;
-							reg1.bit2 = 1;
-						}
-					}
+				if (reg2.bit2 != reg2.bit1)  {
+					reg2.bit2 = ~reg1.bit2; // nyimpen / ga
+					reg1.bit2 = ~reg1.bit2; //update reg1
 				}
-				if (reg2.bit3 == 1) {
-					if (reg1.bit3 == 0) {
-						if (reg2.bit2 == 0) {
-							reg1.bit3 = 1; // update reg1
-						}
-					} else { // reg1.bit2 == 1
-						if (reg2.bit2 == 0) {
-							reg1.bit3 = 0; // nyimpen di reg2
-							reg2.bit3 = 0;
-						}
-					}
-				} else {
-					if (reg2.bit2 == 1) {
-						if (reg1.bit3 == 1) {
-							reg1.bit3 = 0;
-						} else {
-							reg2.bit3 = 1;
-							reg1.bit3 = 1;
-						}
-					}
+				if (reg2.bit3 != reg2.bit2)  {
+					reg2.bit3 = ~reg1.bit3; // nyimpen / ga
+					reg1.bit3 = ~reg1.bit3; //update reg1
 				}
-				if (reg2.bit4 == 1) {
-					if (reg1.bit4 == 0) {
-						if (reg2.bit3 == 0) {
-							reg1.bit4 = 1; // update reg1
-						}
-					} else { // reg1.bit2 == 1
-						if (reg2.bit3 == 0) {
-							reg1.bit4 = 0; // nyimpen di reg2
-							reg2.bit4 = 0;
-						}
-					}
-				} else {
-					if (reg2.bit3 == 1) {
-						if (reg1.bit4 == 1) {
-							reg1.bit4 = 0;
-						} else {
-							reg2.bit4 = 1;
-							reg1.bit4 = 1;
-						}
-					}
+				if (reg2.bit4 != reg2.bit3)  {
+					reg2.bit4 = ~reg1.bit4; // nyimpen / ga
+					reg1.bit4 = ~reg1.bit4; //update reg1
 				}
-				if (reg2.bit5 == 1) {
-					if (reg1.bit5 == 0) {
-						if (reg2.bit4 == 0) {
-							reg1.bit5 = 1; // update reg1
-						}
-					} else { // reg1.bit2 == 1
-						if (reg2.bit4 == 0) {
-							reg1.bit5 = 0; // nyimpen di reg2
-							reg2.bit5 = 0;
-						}
-					}
-				} else {
-					if (reg2.bit4 == 1) {
-						if (reg1.bit5 == 1) {
-							reg1.bit5 = 0;
-						} else {
-							reg2.bit5 = 1;
-							reg1.bit5 = 1;
-						}
-					}
+				if (reg2.bit5 != reg2.bit4)  {
+					reg2.bit5 = ~reg1.bit5; // nyimpen / ga
+					reg1.bit5 = ~reg1.bit5; //update reg1
 				}
-				if (reg2.bit6 == 1) {
-					if (reg1.bit6 == 0) {
-						if (reg2.bit5 == 0) {
-							reg1.bit6 = 1; // update reg1
-						}
-					} else { // reg1.bit2 == 1
-						if (reg2.bit5 == 0) {
-							reg1.bit6 = 0; // nyimpen di reg2
-							reg2.bit6 = 0;
-						}
-					}
-				} else {
-					if (reg2.bit5 == 1) {
-						if (reg1.bit6 == 1) {
-							reg1.bit6 = 0;
-						} else {
-							reg2.bit6 = 1;
-							reg1.bit6 = 1;
-						}
-					}
+				if (reg2.bit6 != reg2.bit5)  {
+					reg2.bit6 = ~reg1.bit6; // nyimpen / ga
+					reg1.bit6 = ~reg1.bit6; //update reg1
 				}
-				if (reg2.bit7 == 1) {
-					if (reg1.bit7 == 0) {
-						if (reg2.bit6 == 0) {
-							reg1.bit7 = 1; // update reg1
-						}
-					} else { // reg1.bit2 == 1
-						if (reg2.bit6 == 0) {
-							reg1.bit7 = 0; // nyimpen di reg2
-							reg2.bit7 = 0;
-						}
-					}
-				} else {
-					if (reg2.bit6 == 1) {
-						if (reg1.bit7 == 1) {
-							reg1.bit7 = 0;
-						} else {
-							reg2.bit7 = 1;
-							reg1.bit7 = 1;
-						}
-					}
-				}
-				if (reg2.bit8 == 1) {
-					if (reg1.bit8 == 0) {
-						if (reg2.bit7 == 0) {
-							reg1.bit8 = 1; // update reg1
-						}
-					} else { // reg1.bit2 == 1
-						if (reg2.bit7 == 0) {
-							reg1.bit8 = 0; // nyimpen di reg2
-							reg2.bit8 = 0;
-						}
-					}
-				} else {
-					if (reg2.bit7 == 1) {
-						if (reg1.bit8 == 1) {
-							reg1.bit8 = 0;
-						} else {
-							reg2.bit8 = 1;
-							reg1.bit8 = 1;
-						}
-					}
+				if (reg2.bit7 != reg2.bit6)  {
+					reg2.bit7 = ~reg1.bit7; // nyimpen / ga
+					reg1.bit7 = ~reg1.bit7; //update reg1
+				} 
+				if (reg2.bit8 != reg2.bit7)  {
+					reg2.bit8 = ~reg1.bit8; // nyimpen / ga
+					reg1.bit8 = ~reg1.bit8; //update reg1
 				}
 			}
 			// + bit1, - bit2, * bit3, / bit4
--- lala.c
+++ lala.c
@@ -1,101 +1,32 @@
-// reg1 += reg2
-				if (reg2.bit1 == 1) {
-					// karena reg2.bit1 udh ga kepake bisa dipake buat nyimpen angka kalo 1 + 1
-					if (reg1.bit1 == 0) {
-						reg1.bit1 == 1; // update reg1
-						reg2.bit1 = 0; // ga nyimpen
-					} else {
-						reg1.bit1 == 0; // nyimpen di reg2.bit1 = 1
-					}
+if (reg3.bit1) { // karena reg3.bit1 udh ga kepake bisa dipake buat nyimpen angka kalo 1 + 1
+					reg3.bit1 = reg2.bit1; // nyimpen / ga
+					reg2.bit1 = ~reg2.bit1; //update reg2
 				}
-				if (reg2.bit2 == 1) {
-					// karena reg2.bit1 udh ga kepake bisa dipake buat nyimpen angka kalo 1 + 1
-					if (reg1.bit2 == 0) {
-						// cek nyimpen apa ga, kalo nyimpen biarin 
-						// krn reg2.bit2 udh bener isinya 1 (nyimpen) terus reg1.bit2 udh bener isinya 0
-						// kalo ga nyimpen brrti update
-						if (reg2.bit1 == 0) {
-							reg1.bit2 == 1; // update reg1
-							reg2.bit2 = 0; // ga nyimpen
-						}
-					} else { // reg1.bit2 == 1
-						// cek nyimpen apa ga, kalo ga brrti kyk biasa
-						if (reg2.bit1 == 0) {
-							reg1.bit2 == 0; // nyimpen di reg2
-						}
-						// kalo nyimpen brrti gaada yg diupdate krn reg2 udh bener nyimpen
-						// terus reg1 udh bener yaitu nilainya 1
-					}
+				if (reg3.bit2 != reg3.bit1)  {
+					reg3.bit2 = reg2.bit2; // nyimpen / ga
+					reg2.bit2 = ~reg2.bit2; //update reg2
 				}
-				if (reg2.bit3 == 1) {
-					if (reg1.bit3 == 0) {
-						if (reg2.bit2 == 0) {
-							reg1.bit3 == 1; // update reg1
-							reg2.bit3 = 0; // ga nyimpen
-						}
-					} else {
-						if (reg2.bit2 == 0) {
-							reg1.bit3 == 0; // nyimpen di reg2
-						}
-					}
+				if (reg3.bit3 != reg3.bit2)  {
+					reg3.bit3 = reg2.bit3; // nyimpen / ga
+					reg2.bit3 = ~reg2.bit3; //update reg2
 				}
-				if (reg2.bit4 == 1) {
-					if (reg1.bit4 == 0) {
-						if (reg2.bit3 == 0) {
-							reg1.bit4 == 1; // update reg1
-							reg2.bit4 = 0; // ga nyimpen
-						}
-					} else { 
-						if (reg2.bit3 == 0) {
-							reg1.bit4 == 0; // nyimpen di reg2
-						}
-					}
+				if (reg3.bit4 != reg3.bit3)  {
+					reg3.bit4 = reg2.bit4; // nyimpen / ga
+					reg2.bit4 = ~reg2.bit4; //update reg2
 				}
-				if (reg2.bit5 == 1) {
-					if (reg1.bit5 == 0) {
-						if (reg2.bit4 == 0) {
-							reg1.bit5 == 1; // update reg1
-							reg2.bit5 = 0; // ga nyimpen
-						}
-					} else {
-						if (reg2.bit4 == 0) {
-							reg1.bit5 == 0; // nyimpen di reg2
-						}
-					}
+				if (reg3.bit5 != reg3.bit4)  {
+					reg3.bit5 = reg2.bit5; // nyimpen / ga
+					reg2.bit5 = ~reg2.bit5; //update reg2
 				}
-				if (reg2.bit6 == 1) {
-					if (reg1.bit6 == 0) {
-						if (reg2.bit5 == 0) {
-							reg1.bit6 == 1; // update reg1
-							reg2.bit6 = 0; // ga nyimpen
-						}
-					} else {
-						if (reg2.bit5 == 0) {
-							reg1.bit6 == 0; // nyimpen di reg2
-						}
-					}
+				if (reg3.bit6 != reg3.bit5)  {
+					reg3.bit6 = reg2.bit6; // nyimpen / ga
+					reg2.bit6 = ~reg2.bit6; //update reg2
 				}
-				if (reg2.bit7 == 1) {
-					if (reg1.bit7 == 0) {
-						if (reg2.bit6 == 0) {
-							reg1.bit7 == 1; // update reg1
-							reg2.bit7 = 0; // ga nyimpen
-						}
-					} else {
-						if (reg2.bit6 == 0) {
-							reg1.bit7 == 0; // nyimpen di reg2
-						}
-					}
-				}
-				if (reg2.bit8 == 1) {
-					if (reg1.bit8 == 0) {
-						if (reg2.bit7 == 0) {
-							reg1.bit8 == 1; // update reg1
-							reg2.bit8 = 0; // ga nyimpen
-						}
-					} else {
-						if (reg2.bit7 == 0) {
-							reg1.bit8 == 0; // nyimpen di reg2
-						}
-					}
+				if (reg3.bit7 != reg3.bit6)  {
+					reg3.bit7 = reg2.bit7; // nyimpen / ga
+					reg2.bit7 = ~reg2.bit7; //update reg2
+				} 
+				if (reg3.bit8 != reg3.bit7)  {
+					reg3.bit8 = reg2.bit8; // nyimpen / ga
+					reg2.bit8 = ~reg2.bit8; //update reg2
 				}
